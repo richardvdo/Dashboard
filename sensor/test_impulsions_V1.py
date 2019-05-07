4cf112e671208670fc1a9f342b2e67361a04876c
richardvdo <richardvdo@gmail.com>
2019-05-07 18:23:01 +0200

    ajout acces bdd

--- sensor/test_impulsions_V1.py
+++ sensor/test_impulsions_V1.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
+import mysql.connector
 
 compteur_principal = 0.0
 compteur = 0
@@ -13,64 +14,67 @@ def cb_compteur_principal(channel):
     global compteur
     global compteur_principal
     global dateJour
-    log = open("/tmp/log_compteur.log", "a")
+    # log = open("/tmp/log_compteur.log", "a")
     now = datetime.datetime.now()
     heure = now.strftime('%Y-%m-%d %H:%M:%S.%f')
-    log.write('boucle de detection %s \n' % int(compteur))
+    # log.write('boucle de detection %s \n' % int(compteur))
     if now.strftime('%d') != dateJour:
-        log.write('if nouveau jour \n')
-        log.write('init connection DB \n')
-        log.write('connecte DB \n')
+        # log.write('if nouveau jour \n')
+        # log.write('init connection DB \n')
+        conn = mysql.connector.connect(host="maison.lithium", user="pi", password="66446644", database="solaire_v1")
+        cursor = conn.cursor()
+        # log.write('connecte DB \n')
         dateJour = now.strftime('%d')
         dateveille = now - datetime.timedelta(1)
         dateveille1 = dateveille.strftime('%Y-%m-%d')
         # timestampveille = datetime.datetime.strptime(dateveille, '%Y-%m-%d').strftime("%s")
         timestampveille = time.mktime(datetime.datetime.strptime(dateveille1, "%Y-%m-%d").timetuple())
         insertline = "insert into solaire_v1.puissance(timestamp, watt) VALUES('%s', '%s')"
-        log.write(insertline)
-        log.write('\n')
+        # log.write(insertline)
+        # log.write('\n')
         var = (timestampveille, float(compteur_principal) / 1000.0)
         new_line = insertline % var
-        log.write(new_line)
-        log.write('\n')
-        # print(new_line)
-        log.write('envoie requete \n')
+        # log.write(new_line)
+        # log.write('\n')
+        # log.write('envoie requete \n')
+        cursor.execute(new_line)
         dateJour = now.strftime('%d')
         compteur_principal = 1
-        log.write('Nouveau jour \n')
-        # conn.commit()
-        # conn.close()
-        log.write('close connection DB \n')
+        compteur = 1
+        # log.write('Nouveau jour \n')
+        conn.commit()
+        conn.close()
+        # log.write('close connection DB \n')
     else:
         compteur_principal = compteur_principal + 1
-        log.write('increment compteur principale \n')
+        # log.write('increment compteur principale \n')
     compteur = compteur + 1
     if compteur >= 100:
-        log.write('if compteur > 100 \n')
-        log.write('init connection DB \n')
-        # conn = mysql.connector.connect(host="maison.lithium", user="pi", password="66446644", database="solaire_v1")
-        # cursor = conn.cursor()
-        log.write('connecte DB \n')
-        line = "compteur principal - %s : %s kW\n" % (heure, float(compteur_principal/1000))
-        log.write(line)
+        # log.write('if compteur > 100 \n')
+        # log.write('init connection DB \n')
+        conn = mysql.connector.connect(host="maison.lithium", user="pi", password="66446644", database="solaire_v1")
+        cursor = conn.cursor()
+        # log.write('connecte DB \n')
+        # line = "compteur principal - %s : %s kW\n" % (heure, float(compteur_principal/1000))
+        # log.write(line)
         # timestamp = int(datetime.datetime.strptime(heure, '%Y-%m-%d %H:%M:%S.%f').strftime("%s"))
-        date = now.strftime('%Y-%m-%d')
-        timestamp = time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())
+        # date = now.strftime('%Y-%m-%d %H:%M:%S')
+        # timestamp = time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").timetuple())
+        timestamp = time.mktime(datetime.datetime.now().timetuple())
         insertline = "insert into solaire_v1.production(timestamp, watt, watt_totale) VALUES('%s',100,'%s')"
-        log.write(insertline)
-        log.write('\n')
+        # log.write(insertline)
+        # log.write('\n')
         var = (timestamp, float(compteur_principal) / 1000.0)
         new_line = insertline % var
-        log.write(new_line)
-        log.write('\n')
-        # print(new_line)
-        log.write('envoie requete \n')
-        # cursor.execute(new_line)
+        # log.write(new_line)
+        # log.write('\n')
+        # log.write('envoie requete \n')
+        cursor.execute(new_line)
         compteur = 0
-        # conn.commit()
-        # conn.close()
-        log.write('close connection DB \n')
-    log.close()
+        conn.commit()
+        conn.close()
+        # log.write('close connection DB \n')
+    # log.close()
 
 
 GPIO.setmode(GPIO.BCM)
--- sensor/test_impulsions_V2.py
+++ sensor/test_impulsions_V2.py
@@ -14,69 +14,42 @@ def cb_compteur_principal(channel):
     global compteur
     global compteur_principal
     global dateJour
-    log = open("/tmp/log_compteur_v3.log", "a")
     now = datetime.datetime.now()
-    heure = now.strftime('%Y-%m-%d %H:%M:%S.%f')
-    log.write('boucle de detection %s \n') % (int(compteur))
     if now.strftime('%d') != dateJour:
-        log.write('if nouveau jour \n')
-        log.write('init connection DB \n')
-        # conn = mysql.connector.connect(host="maison.lithium", user="pi", password="66446644", database="solaire_v1")
-        # cursor = conn.cursor()
-        log.write('connecte DB \n')
+        conn = mysql.connector.connect(host="maison.lithium", user="pi", password="66446644", database="solaire_v1")
+        cursor = conn.cursor()
         dateJour = now.strftime('%d')
-        dateJourComplete = now.strftime('%Y-%m-%d')
-        dateVeille = dateJourComplete - datetime.timedelta(1)
-        timestampVeille = int(datetime.datetime.strptime(dateVeille, '%Y-%m-%d').strftime("%s"))
-        insertline = "insert into puissance.record(timestamp, watt) VALUES('%s', '%s')"
-        log.write(insertline)
-        log.write('\n')
-        var = (timestampVeille, float(compteur_principal) / 1000.0)
-        new_line = (insertline) % (var)
-        log.write(new_line)
-        log.write('\n')
-        # print(new_line)
-        log.write('envoie requete \n')
-        # cursor.execute(new_line)
+        dateveille = now - datetime.timedelta(1)
+        dateveille1 = dateveille.strftime('%Y-%m-%d')
+        timestampveille = time.mktime(datetime.datetime.strptime(dateveille1, "%Y-%m-%d").timetuple())
+        insertline = "insert into solaire_v1.puissance(timestamp, watt) VALUES('%s', '%s')"
+        var = (timestampveille, float(compteur_principal) / 1000.0)
+        new_line = insertline % var
+        cursor.execute(new_line)
+        dateJour = now.strftime('%d')
         compteur_principal = 1
-        log.write('Nouveau jour %s \n') % (int(dateJour))
-        # conn.commit()
-        # conn.close()
-        log.write('close connection DB \n')
+        compteur = 1
+        conn.commit()
+        conn.close()
     else:
         compteur_principal = compteur_principal + 1
-        log.write('increment compteur principale \n')
     compteur = compteur + 1
     if compteur >= 100:
-        log.write('if compteur > 100 \n')
-        log.write('init connection DB \n')
-        # conn = mysql.connector.connect(host="maison.lithium", user="pi", password="66446644", database="solaire_v1")
-        # cursor = conn.cursor()
-        log.write('connecte DB \n')
-        line = "compteur principal - %s : %s kW\n" % (heure, float(compteur_principal/1000))
-        log.write(line)
-        timestamp = int(datetime.datetime.strptime(heure, '%Y-%m-%d %H:%M:%S.%f').strftime("%s"))
+        conn = mysql.connector.connect(host="maison.lithium", user="pi", password="66446644", database="solaire_v1")
+        cursor = conn.cursor()
+        timestamp = time.mktime(datetime.datetime.now().timetuple())
         insertline = "insert into solaire_v1.production(timestamp, watt, watt_totale) VALUES('%s',100,'%s')"
-        log.write(insertline)
-        log.write('\n')
         var = (timestamp, float(compteur_principal) / 1000.0)
-        new_line = (insertline) % (var)
-        log.write(new_line)
-        log.write('\n')
-        # print(new_line)
-        log.write('envoie requete \n')
-        # cursor.execute(new_line)
+        new_line = insertline % var
+        cursor.execute(new_line)
         compteur = 0
-        # conn.commit()
-        # conn.close()
-        log.write('close connection DB \n')
-    log.close()
+        conn.commit()
+        conn.close()
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-6GPIO.add_event_detect(18, GPIO.FALLING, callback=cb_compteur_principal, bouncetime=70)
-
+GPIO.add_event_detect(18, GPIO.FALLING, callback=cb_compteur_principal, bouncetime=70)
 
 while True:
     time.sleep(0.1)
